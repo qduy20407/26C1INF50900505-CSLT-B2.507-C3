c93a436a41a810bc12b44dc4210ac01def3ee935
UEH <UEH@B507PC53>
2026-08-18 16:34:29 +0700

    Add project files.

---
+++ day1/session1/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        NewMethod();
+
+        static void NewMethod()
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("xinchao!");
+            Console.WriteLine("furry gaylord");
+            Console.WriteLine("furry gaylord");
+        }
+    } 
+}
